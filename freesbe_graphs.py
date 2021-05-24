ef0933d21677366a240177597cd143ec8b9b5651
yaelblum <yael.blum.42@gmail.com>
2021-05-24 22:44:17 +0300

    update

--- freesbe_graphs.py
+++ freesbe_graphs.py
@@ -10,7 +10,7 @@ def draw_y_as_x (x,x_name, y, y_name):
     :param y_name: name of y axis
     :return: None
     """
-    plt.plot(x, y)
+    plt.scatter(x, y , s=0.1)
     plt.title(f'{y_name}({x_name})')
     plt.xlabel(x_name)
     plt.ylabel(y_name)
@@ -28,6 +28,6 @@ def draw_xyt(x,x_name, y, y_name, t, t_name):
     :param t_name: t name
     :return: None
     """
-    draw_y_as_x(x, x_name, t, t_name)
-    draw_y_as_x(y, y_name, t, t_name)
-    draw_y_as_x(y, y_name, x, x_name)
+    draw_y_as_x(t, t_name ,x, x_name)
+    draw_y_as_x(t, t_name, y, y_name)
+    draw_y_as_x(x, x_name, y, y_name)
