0df3fbf250278fc0a78d6b4df174795c9ee3dec5
yaelblum <yael.blum.42@gmail.com>
2021-05-31 12:48:09 +0300

    moments

--- 2D_with_pitch.py
+++ 2D_with_pitch.py
@@ -2,7 +2,7 @@ import math
 import numpy as np
 from constants import *
 
-def one_step(u_x_prev, u_z_prev, x_arr, z_arr, theta, theta_1, theta_2, omega):
+def one_step(u_x_prev, u_z_prev, x_arr, z_arr, theta, theta_1, theta_2, phi, phi_1, phi_2, gamma, gamma_1, gamma_2):
     betta = np.arctan(float(u_z_prev[-1]) / float(u_x_prev[-1]))
     alpha = (theta[-1]-betta)
     cl = calc_Cl(alpha)
@@ -15,20 +15,48 @@ def one_step(u_x_prev, u_z_prev, x_arr, z_arr, theta, theta_1, theta_2, omega):
     z_arr.append(z_arr[-1] + u_z_prev[-1] * dt)
 
     betta = np.arctan(float(u_z_prev[-1]) / float(u_x_prev[-1]))
-    theta_2.append((CM0 + CM_alpha * (theta[-1] - betta) + CMq * ((theta[-1] - theta[-2]) / dt)) * 0.5 * Ro * (
-            calc_v_rel(u_x_prev[-1], u_z_prev[-1]) ** 2) * A * (dt ** 2) * d / I_x)
+
+    constants =  0.5 * Ro * (
+            calc_v_rel(u_x_prev[-1], u_z_prev[-1]) ** 2) * A * (dt ** 2) * d
+
+    pitch=(CM0 + CM_alpha * (theta[-1] - betta) + CMq * (theta_1[-1])) *constants
+
+    theta_2.append((pitch + I_z * phi_1[-1] * np.cos(theta[-1])*(phi_1[-1] * np.sin(theta[-1])+ gamma_1[-1])-I_z * phi_1[-1]**2 * np.cos(theta) * np.sin(theta))/ I_x)
+
     theta_1.append(theta_1[-1]+theta_2[-1]*dt)
+
     theta.append((theta[-1]+dt*theta_1[-1])%(np.pi*2))
+
+    roll= (CRr * gamma_1[-1] + CRp* phi_1[-1]) *constants
+
+    phi_2.append(roll + I_x * theta_1[-1] *phi_1[-1 ]* np.sin(theta[-1]) - I_z * theta_1[-1] *( phi_1[-1] * np.sin(theta[-1]) + gamma_1[-1]) + I_x * theta_1[-1] * phi_1 [-1])* np.cos(theta[-1]) / I_x
+
+    phi_1.append(phi_1[-1]+phi_2[-1]*dt)
+
+    phi.append((phi[-1]+dt*phi_1[-1])%(np.pi*2))
+
+    spin_down = (CNr * gamma_1[-1]) * constants
+
+    gamma_2.append( (spin_down - I_z * phi_1[-1] * np.sin( theta) + theta_1[-1] * phi_1[-1] * np.cos(theta[-1]))/I_z)
+    gamma_1.append(gamma_1[-1]+gamma_2[-1]*dt)
+
+    gamma.append((gamma[-1]+dt*gamma_1[-1])%(np.pi*2))
+
+
     # theta.append(((CM0 + CM_alpha * (theta[-1] - betta) + CMq * ((theta[-1] - theta[-2]) / dt)) * 0.5 * Ro * (
     #         calc_v_rel(u_x_prev[-1], u_z_prev[-1]) ** 2) * A * (dt ** 2) * d / I_x) + 2 * theta[-1] - theta[
     #                  -2])
-    omega.append(omega[-1] + dt / I_z * CNr * omega[-1] * 0.5 * Ro * A * d * (
-            calc_v_rel(u_x_prev[-1], u_z_prev[-1]) ** 2))
-    print(omega[-1])
+    # omega.append(omega[-1] + dt / I_z * CNr * omega[-1] * 0.5 * Ro * A * d * (
+    #         calc_v_rel(u_x_prev[-1], u_z_prev[-1]) ** 2))
 
 def basic_simulation(u0_x, u0_z, theta, omega, x_0, z_0):
-    omega= [omega]
-    theta = [theta, theta]
+    gamma = [0]
+    gamma_1=[0]
+    gamma_2=[0]
+    phi = [0]
+    phi_1 = [0]
+    phi_2 = [0]
+    theta = [theta]
     theta_1=[0]
     theta_2=[0]
     x_array = [x_0]
@@ -38,7 +66,7 @@ def basic_simulation(u0_x, u0_z, theta, omega, x_0, z_0):
     t = [0]
 
     while (z_array[-1] > 0):
-        one_step(ux_arr, uz_arr, x_array, z_array, theta,theta_1, theta_2, omega)
+        one_step(ux_arr, uz_arr, x_array, z_array, theta , theta_1, theta_2, phi, phi_1, phi_2, gamma, gamma_1, gamma_2)
         t.append(t[-1] + dt)
     theta.remove(theta[-1])
     return x_array, z_array, t, ux_arr, uz_arr, theta, omega
