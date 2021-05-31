8ea86a195c353031011481a780449133a68f2e66
yaelblum <yael.blum.42@gmail.com>
2021-05-31 13:18:51 +0300

    moments

--- 3D_with_moments.py
+++ 3D_with_moments.py
@@ -3,7 +3,7 @@ import numpy as np
 from constants import *
 
 
-def one_step(u_x_prev, u_z_prev, x_arr, z_arr, theta, theta_1, theta_2, phi, phi_1, phi_2, gamma, gamma_1, gamma_2):
+def one_step(u_x_prev, u_y_prev, u_z_prev, x_arr, y_arr, z_arr, theta, theta_1, theta_2, phi, phi_1, phi_2, gamma, gamma_1, gamma_2):
     betta = np.arctan(float(u_z_prev[-1]) / float(u_x_prev[-1]))
     alpha = (theta[-1] - betta)
     cl = calc_Cl(alpha)
@@ -52,7 +52,7 @@ def one_step(u_x_prev, u_z_prev, x_arr, z_arr, theta, theta_1, theta_2, phi, phi
     #         calc_v_rel(u_x_prev[-1], u_z_prev[-1]) ** 2))
 
 
-def basic_simulation(u0_x, u0_z, theta, x_0, z_0):
+def basic_simulation(u0_x, u0_y, u0_z, theta, x_0, y_0, z_0):
     gamma = [0]
     gamma_1 = [0]
     gamma_2 = [0]
@@ -63,18 +63,23 @@ def basic_simulation(u0_x, u0_z, theta, x_0, z_0):
     theta_1 = [0]
     theta_2 = [0]
     x_array = [x_0]
+    y_array=[y_0]
     z_array = [z_0]
     ux_arr = [u0_x]
+    uy_arr = [u0_y]
     uz_arr = [u0_z]
     t = [0]
 
     while (z_array[-1] > 0):
-        one_step(ux_arr, uz_arr, x_array, z_array, theta, theta_1, theta_2, phi, phi_1, phi_2, gamma, gamma_1, gamma_2)
+        one_step(ux_arr, uy_arr, uz_arr, x_array, y_array, z_array, theta, theta_1, theta_2, phi, phi_1, phi_2, gamma, gamma_1, gamma_2)
         t.append(t[-1] + dt)
-    return  ux_arr, uz_arr, theta, x_array, z_array, t
+    return ux_arr, uy_arr, uz_arr, theta, x_array, y_array, z_array, t
+
 
 if __name__ == '__main__':
-    ux, uz, theta, x, z , t = basic_simulation(20, 0,14 * np.pi / 180, 0, 1.7)
+    ux, uy, uz, theta, x, y, z, t = basic_simulation(10, 0, 0,-8 * np.pi / 180, 0, 0, 1.7)
     draw_xyt(x, 'x', z, 'z', t, 't')
+    draw_y_as_x(t, 't', ux, 'u_x')
+    draw_y_as_x(t, 't', uz, 'u_z')
     draw_y_as_x(t, 't', np.array(theta) * 180 / np.pi, 'theta')
     # comment
